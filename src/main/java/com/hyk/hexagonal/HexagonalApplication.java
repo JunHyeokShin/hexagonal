7a7a0248827be103ed4e8c8d63fea424ac695032
HYK <junhyeok001010@gmail.com>
2026-06-05 01:05:29 +0900

    프로젝트 초기화

---
+++ src/main/java/com/hyk/hexagonal/HexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.hyk.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(HexagonalApplication.class, args);
+  }
+
+}
